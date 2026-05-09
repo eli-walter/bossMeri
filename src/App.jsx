a52eefc4f183deb92584497e90c6b28ff92f079c
eli-walter <elizabethwaltermarket@gmail.com>
2026-05-09 13:29:22 +1100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // src/App.jsx
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import { auth } from './firebase';
 import {
   signInWithEmailAndPassword,
